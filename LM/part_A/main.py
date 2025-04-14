c61d1f37ed45ec5022377b7bd0519f1abe4128d2
Roberto <roberto.savi2582@gmail.com>
2025-04-14 12:01:00 +0200

    Updated learning rate to 0.05 for hidden and embedded sizes

--- LM/part_A/main.py
+++ LM/part_A/main.py
@@ -62,7 +62,7 @@ for model in models:
         best_model_overall = copy.deepcopy(best_model)
 
 # -------------------- Model saving --------------------
-path = 'models/best_hid_emb_model.pt'
+path = 'models/best_hid_emb_model_0_05.pt'
 torch.save(best_model_overall.state_dict(), path)
 
 # To load the model:
@@ -70,7 +70,7 @@ torch.save(best_model_overall.state_dict(), path)
 # model.load_state_dict(torch.load(path))
 
 # -------------------- Save best PPL results --------------------
-with open('results/models_best_hid_emb.txt', 'w') as f:
+with open('results/models_best_hid_emb_0_05.txt', 'w') as f:
     for i, (ppl, model) in enumerate(zip(best_ppls, models)):
         f.write(f'Model {i}: PPL={ppl}, Model ={model}\n')
 
@@ -81,5 +81,5 @@ plt.xlabel('Model index')
 plt.ylabel('Perplexity (PPL)')
 plt.title('Best PPL for each Model-Optimizer configuration')
 plt.legend()
-plt.savefig('images/models_best_hid_emb.jpg')  # Save the plot as an image
+plt.savefig('images/models_best_hid_emb_0_05.jpg')  # Save the plot as an image
 plt.show()
