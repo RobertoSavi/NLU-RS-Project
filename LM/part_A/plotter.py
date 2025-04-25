6684791e2ed99f6d6bafbeeed3724d130ad32d49
Roberto <roberto.savi2582@gmail.com>
2025-04-25 14:44:38 +0200

    Update plotter.py

--- LM/part_A/plotter.py
+++ LM/part_A/plotter.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
-
+# -------------------- Plot PPL results --------------------
 plt.figure(figsize=(10, 5))
 plt.plot(range(len(best_ppls)), best_ppls, marker='o', linestyle='-', label='Best PPL per Model')
 plt.xlabel('Model index')
 plt.ylabel('Perplexity (PPL)')
 plt.title('Best PPL for each Model-Optimizer configuration')
 plt.legend()
-plt.savefig('images/models_LSTM_best_lr.jpg')  # Save the plot as an image
+plt.savefig('images/models_best_lr.jpg')  # Save the plot as an image
 plt.show()
