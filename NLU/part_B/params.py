4bf4e2304cdd0b952a597d98b82dd5e835cae66b
Roberto <roberto.savi2582@gmail.com>
2026-01-27 16:06:22 +0100

    Update params.py

--- NLU/part_B/params.py
+++ NLU/part_B/params.py
@@ -11,7 +11,7 @@ from utils import *
 
 # -------------------- Define hyperparameters for the model --------------------
 n_epochs = 65
-lr = 1e-4
+lr = 0.0001
 batch_size = 128
 dropout = 0.1
 patience_value = 3
